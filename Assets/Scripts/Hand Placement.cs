2bbe2628d974f63d570260a420db001a66c90127
KRESTONS-PC\ABEL4 <abel4prezz@gmail.com>
2025-09-14 20:09:54 -0700

    Sketchy code, half baked

--- Assets/Scripts/Hand Placement.cs
+++ Assets/Scripts/Hand Placement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HandPlacement : MonoBehaviour
 {
@@ -10,11 +11,19 @@ public class HandPlacement : MonoBehaviour
     public Transform pointAClose;
     public bool gripping;
 
+    //positions
     private float _positionX;
     private float _positionY;
 
+    //mouse movement components
+    Vector2 pos = new Vector2(0f, 3f);
+    public Transform targetsEmpty;
+    public float sensitivity = .5f;
 
-
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     private void Update()
     {
         if (pointAOpen != null && pointBOpen != null && !gripping)
@@ -38,6 +47,19 @@ public class HandPlacement : MonoBehaviour
 
             //setting the position of the part
             transform.position = new Vector2(_positionX, _positionY);
+        }
+
+        //Move the position of the targets
+        if (targetsEmpty != null)
+        {
+            pos.x += Input.GetAxis("Mouse X") * sensitivity;
+            pos.y += Input.GetAxis("Mouse Y") * sensitivity;
+            targetsEmpty.position = Vector2.ClampMagnitude(pos, 4f);
+            pos = Vector2.ClampMagnitude(pos, 4f);
+            targetsEmpty.position = Vector2.ClampMagnitude(pos, 4f);
+            Debug.Log(pos);
+            Debug.Log(targetsEmpty);
+            Mouse.current.WarpCursorPosition(targetsEmpty.position);
         }
     }
 }
